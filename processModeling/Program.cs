9ed561b49064355309a7add25877f514d77ef1d6
Юля <kramarenko.julia@unicyb.kiev.ua>
2019-04-16 23:30:07 +0300

    Added a Windows form, Process constructer, generated sensor from given range

--- processModeling/Program.cs
+++ processModeling/Program.cs
@@ -16,7 +16,7 @@ namespace processModeling
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormMM());
         }
     }
 }
